f886b7719e04004228b9ddb19883150dd17a96bf
Will Morris <morris.william.work@gmail.com>
2025-05-17 15:15:46 -0400

    Add persistent frontiers state

--- src/main/net/williserver/frontiers/model/FrontiersModel.kt
+++ src/main/net/williserver/frontiers/model/FrontiersModel.kt
@@ -1,6 +1,10 @@
 package net.williserver.frontiers.model
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import net.williserver.frontiers.LogHandler
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
 
 /**
  * Data structure for information about frontiers.
@@ -8,7 +12,7 @@ import net.williserver.frontiers.LogHandler
  * @property currentTier The current tier level of the frontier, represented as an unsigned integer.
  */
 @Serializable
-data class FrontiersData(val currentTier: UInt)
+data class FrontiersData(val currentTier: UInt = FrontiersModel.DEFAULT_TIER)
 
 /**
  * Data model for the Frontiers plugin. Represents what frontier we're in.
@@ -27,8 +31,53 @@ class FrontiersModel(data: FrontiersData, logger: LogHandler) {
         tier
     }
 
+    /**
+     * @return a serializable `FrontiersData` object containing the current tier of the model.
+     */
+    fun asDataTuple() = FrontiersData(currentTier)
+
+    /*
+     * Comparison helpers
+     */
+    override fun equals(other: Any?) = other is FrontiersModel && this.currentTier == other.currentTier
+
+    override fun hashCode() = currentTier.hashCode()
+
+    override fun toString() = "FrontiersModel(currentTier=$currentTier)"
+
+    /*
+     * Static state
+     */
     companion object {
         // By default, the model starts at tier one.
         const val DEFAULT_TIER = 1u
+
+        /**
+         * @param path the file path from which to read the frontier data.
+         * @param logger the logger used for logging error and informational messages.
+         * @return the deserialized frontier data if the file exists, or a new instance of `FrontiersData` if the file does not exist.
+         */
+        fun readFromFile(path: String, logger: LogHandler): FrontiersData =
+            if (!File(path).exists()) {
+                logger.err("Frontiers file $path does not exist, creating new one.")
+                FrontiersData()
+            } else {
+                val reader = FileReader(path)
+                val jsonString = reader.readText()
+                reader.close()
+                Json.decodeFromString<FrontiersData>(jsonString)
+            }
+
+        /**
+         * Writes the serialized representation of the given `FrontiersModel` to the specified file path.
+         *
+         * @param model the `FrontiersModel` instance to be serialized and written to the file.
+         * @param path the file path where the serialized data will be written.
+         */
+        fun writeToFile(model: FrontiersModel, path: String) {
+            val writer = FileWriter(path)
+            writer.write(Json.encodeToString(model.asDataTuple()))
+            writer.close()
+        }
     }
 }
--- src/test/net/williserver/frontiers/model/FrontiersModelTest.kt
+++ src/test/net/williserver/frontiers/model/FrontiersModelTest.kt
@@ -4,8 +4,17 @@ import net.williserver.frontiers.LogHandler
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-
+/**
+ * @author Willmo3
+ */
 class FrontiersModelTest {
+    /**
+     * Validates the initialization behavior of the `FrontiersModel` class.
+     *
+     * This test ensures that:
+     * - A `FrontiersModel` initialized with a tier below the default tier will set the current tier to the default tier.
+     * - A `FrontiersModel` initialized with a tier above the default tier correctly sets the current tier to the specified value.
+     */
     @Test
     fun FrontiersModelInitTest() {
         val belowDefaultModel = FrontiersModel(FrontiersData(0u), LogHandler(null))
@@ -13,5 +22,22 @@ class FrontiersModelTest {
 
         val aboveDefaultModel = FrontiersModel(FrontiersData(100u), LogHandler(null))
         assertEquals(100u, aboveDefaultModel.currentTier)
+    }
+
+    /**
+     * Tests the ability to persist and restore the state of a `FrontiersModel` instance.
+     *
+     * This method performs the following:
+     * 1. Creates a `FrontiersModel` instance with an initial `currentTier` value.
+     * 2. Writes the model's state to a file using `FrontiersModel.writeToFile`.
+     * 3. Reads the serialized model data back from the file using `FrontiersModel.readFromFile`.
+     * 4. Verifies that the restored model maintains the same `currentTier` value as the original.
+     */
+    @Test
+    fun testReadWriteFrontier() {
+        val model = FrontiersModel(FrontiersData(100u), LogHandler(null))
+        FrontiersModel.writeToFile(model, "test.json")
+        val newModel = FrontiersModel(FrontiersModel.readFromFile("test.json", LogHandler(null)), LogHandler(null))
+        assertEquals(model, newModel)
     }
 }
